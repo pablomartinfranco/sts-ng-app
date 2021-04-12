ea980c3853326b4e3289e9ed3f2978c355c405c2
ichi <ichi@DESKTOP-KH5G72L>
2021-04-11 21:49:20 -0300

    ya perdi la paciencia

--- src/main/java/challenge/ChallengeApplication.java
+++ src/main/java/challenge/ChallengeApplication.java
@@ -1,36 +1,13 @@
 package challenge;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 
-@RestController
 @SpringBootApplication
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
-	}
-	
-	@GetMapping("/")
-  	public HttpStatus home() {
-		return HttpStatus.OK;
-	}
-
-	@GetMapping("/resource")
-  	public Map<String, Object> resource() {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
 	}
 
 }
