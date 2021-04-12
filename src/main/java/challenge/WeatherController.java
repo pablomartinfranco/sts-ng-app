14898cc4f389b8e4c9226feaa8ee4636a15d5a9f
ichi <ichi@DESKTOP-KH5G72L>
2021-04-11 21:50:01 -0300

    esta vivo

---
+++ src/main/java/challenge/WeatherController.java
@@ -0,0 +1,21 @@
+package challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherController {
+	
+	@GetMapping("/resource")
+  	public Map<String, Object> resource() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+	
+}
