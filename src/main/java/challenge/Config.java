37d2088d655031d5fda25c6fc9dedd19deb202c8
ichi <ichi@DESKTOP-KH5G72L>
2021-04-12 01:16:42 -0300

    logging added

--- src/main/java/challenge/Config.java
+++ src/main/java/challenge/Config.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 @ComponentScan("services")
@@ -14,9 +15,20 @@ public class Config {
     @Bean
     public HttpClient httpClient() {
     	return HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
+	        .version(HttpClient.Version.HTTP_1_1)
+	        .connectTimeout(Duration.ofSeconds(10))
+	        .build();
+    }
+    
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
     }
 
 }
